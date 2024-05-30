5bbc79686a99409c438fe7b142dcda5c0a410328
ReyLuk <faxther18@gmail.com>
2024-05-29 22:43:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['447700168473',
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
+global.botNumberCode = '573208154296' //Ejemplo: +59309090909
 global.confirmCode = ''
 
 global.suittag = ['5214531287294']
